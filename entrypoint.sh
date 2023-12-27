770088ad7029e785e74160fd3be68dd3e24e1663
frknkrc44 <krc440002@gmail.com>
2023-12-28 01:13:23 +0300

    Print flutter version

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,9 @@ export FLUTTER_ROOT=/opt/flutter
 export PUB_CACHE=/var/tmp/.pub_cache
 export PATH=$FLUTTER_ROOT/bin:$PUB_CACHE/bin:$PATH
 git config --global --add safe.directory $FLUTTER_ROOT
+flutter --no-version-check --version --machine
 
-export JSON_OUTPUT=$(pana --no-warning $GITHUB_WORKSPACE --json)
+export JSON_OUTPUT=$(pana --no-warning --flutter-sdk $FLUTTER_ROOT $GITHUB_WORKSPACE --json)
 
 export SUMMARY_CREATOR=$GITHUB_WORKSPACE/generate_pana_summary.sh
 chmod +x $SUMMARY_CREATOR
