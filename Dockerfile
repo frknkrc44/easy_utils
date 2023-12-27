d4403f81b686ce32a2168d7f3b9de97005bf5149
frknkrc44 <krc440002@gmail.com>
2023-12-28 01:00:13 +0300

    Try to fix PATH error

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ COPY entrypoint.sh /entrypoint.sh
 RUN apk update && \
     apk add jq && \
     dart pub global activate pana && \
+    dart pub global activate dartdoc && \
     chmod +x /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 
 export FLUTTER_ROOT=/opt/flutter
 export PUB_CACHE=/var/tmp/.pub_cache
-export PATH=$PATH:$FLUTTER_ROOT/bin:$PUB_CACHE/bin
+export PATH=$FLUTTER_ROOT/bin:$PUB_CACHE/bin:$PATH
 git config --global --add safe.directory $FLUTTER_ROOT
 
-export JSON_OUTPUT=$(pana --no-warning --flutter-sdk $FLUTTER_ROOT $GITHUB_WORKSPACE --json)
+export JSON_OUTPUT=$(pana --no-warning $GITHUB_WORKSPACE --json)
 
 export SUMMARY_CREATOR=$GITHUB_WORKSPACE/generate_pana_summary.sh
 chmod +x $SUMMARY_CREATOR
