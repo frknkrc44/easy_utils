eb9df069b2e44f8882dc5fbf367cb0f59b21f77d
frknkrc44 <krc440002@gmail.com>
2024-05-31 01:45:33 +0300

    Remove unneeded length parameter

--- lib/src/network/http_file.dart
+++ lib/src/network/http_file.dart
@@ -21,30 +21,13 @@ class HttpFile {
   /// The content/mime type of the file.
   final String mimeType;
 
-  /// The byte length of the file.
-  final int length;
-
   /// The main constructor of the [HttpFile].
   HttpFile({
     required this.name,
     required this.bytes,
     required this.mimeType,
-    required this.length,
   });
-
-  /// Create a [HttpFile] instance without length.
-  factory HttpFile.fromNameAndBytes(
-    String name,
-    Uint8List bytes,
-    String mimeType,
-  ) =>
-      HttpFile(
-        name: name,
-        bytes: bytes,
-        mimeType: mimeType,
-        length: bytes.length,
-      );
-
+  
   /// Export the [HttpFile] as the [http.MultipartFile].
   http.MultipartFile asMultipartFile(String field) =>
       http.MultipartFile.fromBytes(
